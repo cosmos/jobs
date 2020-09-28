8004aaae30cd5d4eafdb3d47ed48f0dba7ef9ac2
Todd Santoro <todd@toddsantoro.com>
2020-09-28 10:17:57 -0400

    added some error reporting to debug the email send on claim a project

--- wp-content/themes/front-child/inc/claim-project-email-form.php
+++ wp-content/themes/front-child/inc/claim-project-email-form.php
@@ -41,3 +41,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
       http_response_code(403);
       echo "There was a problem with your submission, please try again.";
   }
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
