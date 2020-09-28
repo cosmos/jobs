5639c20db7429dd27d6dfdfb153578dbc116065a
Todd Santoro <todd@toddsantoro.com>
2020-09-28 10:50:53 -0400

    more debugging with this server.

--- wp-content/themes/front-child/inc/claim-project-email-form.php
+++ wp-content/themes/front-child/inc/claim-project-email-form.php
@@ -41,4 +41,4 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 //     # Not a POST request, set a 403 (forbidden) response code.
 //     http_response_code(403);
 //     echo "There was a problem with your submission, please try again.";
-// }
+}
