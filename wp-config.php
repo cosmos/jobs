5dedda2bce74508df7ec16df984e95c119ad3833
Todd Santoro <todd@promiseprotocols.com>
2020-10-14 14:23:03 +0000

    debug setting

--- wp-config.php
+++ wp-config.php
@@ -70,7 +70,7 @@ $table_prefix = 'wp_';
  * "true" in dev, but false in test and live.
  */
 if ( ! defined( 'WP_DEBUG' ) ) {
-	define('WP_DEBUG', true);
+	define('WP_DEBUG', fasle);
 }
 
 /* That's all, stop editing! Happy Pressing. */
